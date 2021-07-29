f89b49ee564de6356f7d03d9c659b2c4f4baa076
mineteeraa <patteera_mine@hotmail.com>
2021-07-29 22:41:57 +0700

    Add progress bar and create prev-page, next-page

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,14 @@ import AirlineDetails from '@/views/details/AirlineDetails.vue'
 import NotFound from '@/views/NotFound.vue'
 import NetworkError from '@/views/NetworkError.vue'
 import EditPassengerDetails from '@/views/details/EditPassengerDetails.vue'
+import NProgress from 'nprogress'
 
 const routes = [
   {
     path: '/',
     name: 'PassengerList',
     component: PassengerList,
-    props: (route) => ({ page: parseInt(route.query.page) || 0, size: parseInt(route.query.size) || 10 })
+    props: (route) => ({ page: parseInt(route.query.page) || 0, size: parseInt(route.query.size) || 5 })
   },
   {
     path: '/about',
@@ -66,6 +67,12 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+router.beforeEach(() => {
+  NProgress.start()
+})
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
--- src/views/PassengerList.vue
+++ src/views/PassengerList.vue
@@ -6,6 +6,24 @@
       :key="passenger.id"
       :passenger="passenger"
     />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'PassengerList', query: { page: page - 1, size } }"
+        rel="prev"
+        v-if="page != 0"
+      >
+        Prev Page</router-link
+      >
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'PassengerList', query: { page: page + 1, size } }"
+        rel="next"
+      >
+        Next Page</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -13,7 +31,7 @@
 // @ is an alias to /src
 import PassengerCard from '@/components/PassengerCard.vue'
 import PassengerService from '@/services/PassengerService.js'
-import { watchEffect } from '@vue/runtime-core'
+
 // import axios from 'axios'
 
 export default {
@@ -37,22 +55,39 @@ export default {
       totalPassengers: 0 // <--- Added this to store totalEvents
     }
   },
-  created() {
-    watchEffect(() => {
-      PassengerService.getEvents(this.page, this.size)
-        .then((response) => {
-          this.passengers = response.data.data
-          this.totalPassengers = response.headers['x-total-count'] // <--- Store it
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    PassengerService.getEvents(
+      parseInt(routeTo.query.page) || 0,
+      parseInt(routeTo.query.size) || 5
+    )
+      .then((response) => {
+        next((comp) => {
+          comp.passengers = response.data.data
+          comp.totalPassengers = response.headers['x-total-count'] // <--- Store it
         })
-        .catch((error) => {
-          console.log(error)
-        })
-    })
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    PassengerService.getEvents(
+      parseInt(routeTo.query.page) || 0,
+      parseInt(routeTo.query.size) || 5
+    )
+      .then((response) => {
+        this.passengers = response.data.data
+        this.totalPassengers = response.headers['x-total-count'] // <--- Store it
+        next()
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
   },
   computed: {
     hasNextPage() {
       //First, calculate total pages
-      let totalPages = Math.ceil(this.totalPassengers / this.size) // 2 is events per page
+      let totalPages = Math.ceil(this.totalPassengers / this.size)
 
       //Then check to see if the current page is less than the total pages.
       return this.page < totalPages
@@ -60,10 +95,30 @@ export default {
   }
 }
 </script>
+
 <style scoped>
 .passengers {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
